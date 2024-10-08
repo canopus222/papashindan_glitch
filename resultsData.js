a11b1815c56b445c4b571358b78daebf55e2e610
Glitch (jagged-furtive-shear) <none>
2024-10-08 06:29:09 +0000

    🥺❔ Updated with Glitch

---
+++ resultsData.js
@@ -0,0 +1,38 @@
+const resultsData = [
+  {
+    number: '5',
+    content: 'スーパーパパ',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZbyFzKmTUzJg_jVoHOgyjOdkMK6773UNQ6FLX1BYa8EmSCK3RzgLypFz-vo__cYcbIjdpifIvRYoABh0NExeBVesi8Jvb60QlQOlm8hY4iEekxgTTsq54ehxZ2NxVTWP41uv9dsP5IjO-/s800/hero_man.png',
+    description: 'いつも子供のことを考えて、積極的に子育てに関わるパパ。責任感があり、子供との時間を大切にし、何事にも全力で取り組む父親像です。▶パートナーは安心し、これからも一緒に育てていけることに喜びと幸せを感じています。',
+  },
+  {
+    number: '4',
+    content: '思いやりパパ',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2qbSjaO0a1KB0QwoWORZblMWM59PBpz7hzrGhtucbkR4UoF27y9Ov1_ISdTgPj-jH-pLg0OJ1xbpsqMOSS2Lk1j_aC6imwljPl8OVpcNAsVbR2_7SpWJPlEzoH-Oy907iGBzkvf7a1Ixr/s1600/baby_jyunyu_honyubin_man.png',
+    description: '子供に対して思いやりを持ち、状況に応じてサポートするパパ。忙しい時もあるけど、できる限り子供に寄り添おうとしています。▶子供ができないことにすぐイライラせず、何事もはじめ出来ないのは当たり前。仏のような寛大な心をもち最高の父親を目指しましょう。',
+  },
+  {
+    number: '3',
+    content: 'バランス型パパ',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgY5ldxEgyZkoE8Y63WkTFbjmFeDu1dITSme6Ek5uUSr8xiG_3Cs-c6RfuNByiauwAEZQ607XEHxRDg8MyvL44rFQudskrpZpE7cJv0Hliphqb9b8EdBsdDTinkrDAfJEPky6oVkP_RoyKX/s800/baby_dakko_father.png',
+    description: '時々子育てに関与し、他のこととバランスを取りながら子供に接しています。子供との関係も良好で、必要な時にはしっかりとサポートするタイプです。▶家事ばかりではなく、しっかりこどもとも触れ合うことを継続しましょう。',
+  },
+  {
+    number: '2',
+    content: '頑張りパパ',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglWaIhVJLtnBYiOV_PIfjIxu1JfQFRyTCwDO5XlMAQQMY6NpjhcCd6-B1U8g4cAMwk-CrE6gQ8aWbWRx3uplfoEIetkWkrmUuvlXNdjZN80FAf3Uw69aLH1Q2hUMbjBbzng6R_8GjHkHz4/s1600/shitsuke_shikaru_father_mu.png',
+    description: '子育てに苦戦することも多いですが、努力はしています。まだ改善の余地はありますが、少しずつ親としての成長を目指しています。▶外部のやり方を実行し、はなたかくアピールするのではなく、パートナーのやり方をすぐ隣で覚えて実行することをまずは優先させましょう。',
+  },
+  {
+    number: '1',
+    content: '放任パパ',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj1ibgWldk5frzbYyvMRZtQWV0S12CU287M3djxDTgQ0A_7I3285IYTOI5lrfYZPo4Tbnv0FXzZDe3J3i2VmNqivX4UO4vaAEYinlqQ5bSPPNSmAczMWG1Gug_Sk1CYGiJ5PfmhUa2pJFtr/s800/smartphone_man_walk.png',
+    description: '子育てにあまり関心がなく、パートナーに任せきりのタイプ。子供との関わりを増やすための努力が必要です。▶おむつとか、熱とか、食事とか。経験がなくてわからなかった？は？わかろうとしてないの間違いではありませんか？あなたのその幼稚な考えがこどもの命取りなのです。パートナーは一緒にやっていく自信を完全になくしていますよ。',
+  },
+  {
+    number: '0',
+    content: 'パパでもない論外',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgIS48YfZcp9P564oyplQJ1OUGyiyxC1VOND9XPKWzdVvr2YI9mPobletKMogCtf4Cp0wm5h3yaum0pMw4AraW3p-pxvcXMj5zXjKZbAaQzwyKYNsJKaALXUoG4RTLnW5zcX22MWZlc-gkg/s800/gomidashi_woman.png',
+    description: 'パートナーは完全に吹っ切れています。今後の展開はご自身で自由に想像してください。',
+  }
+];
